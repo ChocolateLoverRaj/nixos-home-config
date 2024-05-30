100cc67df7a5bb11fec08a856071ece7247f4cca
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-05-30 15:14:07 -0700

    Clean up plasma manager config

--- plasma.nix
+++ plasma.nix
@@ -6,19 +6,6 @@
 
     workspace = {
       theme = "breeze-dark";
-      colorScheme = "BreezeDark";
-      lookAndFeel = "org.kde.breezedark.desktop";
     };
-
-    # shortcuts = {
-    #   "services/tabby.desktop"."_launch" = "Ctrl+Alt+T";
-    #   "services/org.kde.konsole.desktop"."_launch" = [ ];
-    # };
-
-    # configFile = {
-    #   # "kdeglobals"."General"."TerminalApplication".value = "warp-terminal %U";
-    #   "kdeglobals"."General"."TerminalService".value = "tabby.desktop";
-    #   "kcminputrc"."Libinput/1267/12859/ELAN2703:00 04F3:323B Touchpad"."NaturalScroll".value = true;
-    # };
   };
 }
