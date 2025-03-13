351ffe8b7f459980267f7aa65cce4b311d9d5371
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-03-13 14:20:47 -0700

    update vscode config

--- vscodium.nix
+++ vscodium.nix
@@ -4,32 +4,34 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      rust-lang.rust-analyzer
-      mkhl.direnv
-      ms-vscode.cpptools-extension-pack
-      ms-vscode-remote.remote-ssh
-    ];
-    keybindings = [
-      {
-        key = "ctrl+t";
-        command = "explorer.newFile";
-      }
-    ];
-    userSettings = {
-      "editor.formatOnSave" = true;
-      "git.autofetch" = true;
-      "git.confirmSync" = false;
-      "workbench.editor.wrapTabs" = true;
-      "tabnine.experimentalAutoImports" = true;
-      "git.enableSmartCommit" = true;
-      "rust-analyzer.procMacro.attributes.enable" = true;
-      "git.allowForcePush" = true;
-      "terminal.integrated.profiles.linux" = {
-        "bash" = null;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        rust-lang.rust-analyzer
+        mkhl.direnv
+        ms-vscode.cpptools-extension-pack
+        ms-vscode-remote.remote-ssh
+      ];
+      keybindings = [
+        {
+          key = "ctrl+t";
+          command = "explorer.newFile";
+        }
+      ];
+      userSettings = {
+        "editor.formatOnSave" = true;
+        "git.autofetch" = true;
+        "git.confirmSync" = false;
+        "workbench.editor.wrapTabs" = true;
+        "tabnine.experimentalAutoImports" = true;
+        "git.enableSmartCommit" = true;
+        "rust-analyzer.procMacro.attributes.enable" = true;
+        "git.allowForcePush" = true;
+        "terminal.integrated.profiles.linux" = {
+          "bash" = null;
+        };
+        "git.openRepositoryInParentFolders" = "always";
       };
-      "git.openRepositoryInParentFolders" = "always";
     };
   };
 }
