d24ef8ff9356eb3186cc4ed06c9ae3f3d5ac545c
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-18 14:24:59 -0800

    Add probe-rs Vscode

--- modules/development.nix
+++ modules/development.nix
@@ -35,6 +35,12 @@
             version = "1.7.5";
             hash = "sha256-SYYWfVMEjwBWXLcFp8rDmqhXCef7ixrrxpdcybJXLGg=";
           }
+          {
+            name = "probe-rs-debugger";
+            publisher = "probe-rs";
+            version = "0.24.2";
+            hash = "sha256-1Gs5D5was/ZOZQrVD3/UiRm5bb12vGIl60AZV2EnazQ=";
+          }
         ];
       keybindings = [
         {
