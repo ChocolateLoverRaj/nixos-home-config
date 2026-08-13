d65af6405ab7ba689f8a8705869c050d3550c749
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-08-12 19:31:00 -0700

    Switch back to unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
